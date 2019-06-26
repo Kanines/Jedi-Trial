28900f0c28ce9c2216cf6cf94bc59fcce47cfa7f
Kanines <kamilbielecki10@gmail.com>
2019-06-27 01:40:11 +0200

    Added Cinemachine camera & improved Player jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,10 +7,9 @@ public class Player : MonoBehaviour
     private Rigidbody2D _rigid;
     [SerializeField]
     private float _jumpForce = 5.0f;
+    private bool _resetJump = false;
     [SerializeField]
-    private bool _isGrounded = false;
-    [SerializeField]
-    private LayerMask _groundLayer;
+    private float _horizSpeed = 5.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -21,25 +20,42 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Horizontal movement
-        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        Movement();
+    }
 
-        if (Input.GetKeyDown(KeyCode.Space) && _isGrounded)
-        {
-            _rigid.velocity = new Vector2(_rigid.velocity.x, _jumpForce);
-            _isGrounded = false;
-        }
+    void Movement()
+    {
+        // Horizontal Movement
+        float horizInput = Input.GetAxisRaw("Horizontal");
+        _rigid.velocity = new Vector2(horizInput * _horizSpeed, _rigid.velocity.y);
 
         // Jump
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        {
+            _rigid.velocity = new Vector2(_rigid.velocity.x, _jumpForce);
+            StartCoroutine(ResetJumpRoutine());
+        }
+    }
+
+    bool IsGrounded()
+    {
         RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.down, 1.05f, 1 << 8);
-        Debug.DrawRay(transform.position, Vector2.down * 1.05f, Color.red);
+        //Debug.DrawRay(transform.position, Vector2.down * 1.05f, Color.red);
 
         if (hitInfo.collider != null)
         {
-            Debug.Log(hitInfo.collider.name);
-            _isGrounded = true;
+            //Debug.Log(hitInfo.collider.name);
+            if (_resetJump == false)
+                return true;
         }
 
-        _rigid.velocity = new Vector2(horizontalInput, _rigid.velocity.y);
+        return false;
+    }
+
+    IEnumerator ResetJumpRoutine()
+    {
+        _resetJump = true;
+        yield return new WaitForSeconds(0.1f);
+        _resetJump = false;
     }
 }
