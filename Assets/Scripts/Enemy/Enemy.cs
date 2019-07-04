e44dc972aa17c428720f4fa4304db9ee4ade46d0
Kanines <kamilbielecki10@gmail.com>
2019-07-05 01:16:21 +0200

    Added Player basic Attack Hitbox collision & Refactored assets

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -6,18 +6,72 @@ public abstract class Enemy : MonoBehaviour
 {
     [SerializeField]
     protected int health;
-    [SerializeField]    
+    [SerializeField]
     protected int speed;
     [SerializeField]
-    protected int reward;
+    protected int rewardPoints;
     [SerializeField]
     protected Transform pointA, pointB;
 
-    public virtual void Attack()
-    {
+    protected Vector3 pathTarget;
+    protected Animator anim;
+    protected SpriteRenderer sprite;
 
+    private void Start()
+    {
+        Init();
+        pathTarget = pointA.position;
     }
 
-    public abstract void Update();
+    public virtual void Init()
+    {
+        anim = GetComponentInChildren<Animator>();
+        sprite = GetComponentInChildren<SpriteRenderer>();
+    }
 
+
+    public virtual void Update()
+    {
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+        {
+            return;
+        }
+
+        Movement();
+    }
+
+    public virtual void Movement()
+    {
+        if (pathTarget == pointA.position)
+        {
+            if (sprite.flipX == false)
+            {
+                sprite.flipX = true;
+                sprite.transform.position = new Vector3(sprite.transform.position.x - sprite.sprite.bounds.size.x,
+                        sprite.transform.position.y, sprite.transform.position.z);
+            }
+        }
+        else
+        {
+            if (sprite.flipX)
+            {
+                sprite.flipX = false;
+                sprite.transform.position = new Vector3(sprite.transform.position.x + sprite.sprite.bounds.size.x,
+                        sprite.transform.position.y, sprite.transform.position.z);
+            }
+        }
+
+        if (transform.position == pointA.transform.position)
+        {
+            pathTarget = pointB.position;
+            anim.SetTrigger("Idle");
+        }
+        else if (transform.position == pointB.transform.position)
+        {
+            pathTarget = pointA.position;
+            anim.SetTrigger("Idle");
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, pathTarget, speed * Time.deltaTime);
+    }
 }
--- Assets/Scripts/Enemy/Tusken.cs
+++ Assets/Scripts/Enemy/Tusken.cs
@@ -4,64 +4,10 @@ using UnityEngine;
 
 public class Tusken : Enemy
 {
-    private Vector3 _pathTarget;
-    private Animator _anim;
-    private SpriteRenderer _tuskenSprite;
-
-    // Start is called before the first frame update
-    void Start()
+    public override void Init()
     {
-        _pathTarget = pointA.position;
-        _anim = GetComponentInChildren<Animator>();
-        _tuskenSprite = GetComponentInChildren<SpriteRenderer>();
-    }
-
-    public override void Attack()
-    {
-
-    }
-
-    // Update is called once per frame
-    public override void Update()
-    {
-        if (_anim.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
-            return;
-
-        Movement();
-    }
-
-    private void Movement()
-    {
-        if (_pathTarget == pointA.position)
-        {
-            if (_tuskenSprite.flipX == false)
-            {
-                _tuskenSprite.flipX = true;
-                _tuskenSprite.transform.position = new Vector3(_tuskenSprite.transform.position.x - _tuskenSprite.sprite.bounds.size.x,
-                        _tuskenSprite.transform.position.y, _tuskenSprite.transform.position.z);
-            }
-        }
-        else
-        {
-            if (_tuskenSprite.flipX)
-            {
-                _tuskenSprite.flipX = false;
-                _tuskenSprite.transform.position = new Vector3(_tuskenSprite.transform.position.x + _tuskenSprite.sprite.bounds.size.x,
-                        _tuskenSprite.transform.position.y, _tuskenSprite.transform.position.z);
-            }
-        }
-
-        if (transform.position == pointA.transform.position)
-        {
-            _pathTarget = pointB.position;
-            _anim.SetTrigger("Idle");
-        }
-        else if (transform.position == pointB.transform.position)
-        {
-            _pathTarget = pointA.position;
-            _anim.SetTrigger("Idle");
-        }
-
-        transform.position = Vector3.MoveTowards(transform.position, _pathTarget, speed * Time.deltaTime);
+        base.Init();
+        health = 5;
+        speed = 3;
     }
 }
