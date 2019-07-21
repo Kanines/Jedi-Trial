46ab7b2d7bcb87941b9983ca6d565d5e9da7af82
Kanines <kamilbielecki10@gmail.com>
2019-07-22 01:15:18 +0200

    Added Trooper Bullet

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5.0f;
+    private Rigidbody2D _rigid;
+    
+    void Start()
+    {
+        _rigid.velocity = transform.right * speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        Player player = other.GetComponent<Player>();
+        if(player != null)
+        {
+            player.Damage(1);
+        }
+        Destroy(gameObject);
+    }
+}
